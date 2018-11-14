acfe296e601300cddd7f92904a7d7a73facca726
Marc MacLeod <marbemac@gmail.com>
2018-11-14 02:55:19 -0500

    feat: add level prop to IValidation
    yaml parser reports validation levels

--- validations/index.d.ts
+++ validations/index.d.ts
@@ -1,11 +1,13 @@
+import { LogLevel } from '../logs';
 import { IPathPosition } from '../parsers';
 
 export interface IValidation {
   ruleId: string;
 
+  level?: LogLevel;
+
   /** Starting line of error */
   line?: number;
-
   column?: number;
   location?: IPathPosition;
 }
