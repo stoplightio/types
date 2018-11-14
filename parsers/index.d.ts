77530f544a7a972e4ec13646241ef955e4f933db
Marc MacLeod <marbemac@gmail.com>
2018-11-14 02:13:28 -0500

    add parser interfaces
    this is needed by our yaml and json source map parser packages

---
+++ parsers/index.d.ts
@@ -0,0 +1,23 @@
+import { IValidation } from '../validations';
+
+export type SourceMapParser<T = any> = (value: string) => IParserResult<T>;
+
+export interface IPosition {
+  /** line number (should start at 1) */
+  line: number;
+}
+
+export interface IPathPosition {
+  start: IPosition;
+  end?: IPosition;
+}
+
+export interface IParserResultPointers {
+  [path: string]: IPathPosition;
+}
+
+export interface IParserResult<T = any> {
+  data: T;
+  pointers: IParserResultPointers;
+  validations: IValidation[];
+}
