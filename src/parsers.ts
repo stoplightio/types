84da2a487b4b92e8a6e21e593a8eb146501e7535
Jakub Rożek <P0lip@users.noreply.github.com>
2019-03-21 04:10:18 +0100

    feat: update diagnostics and parsing interfaces BREAKING [SL-2114] (#27)
    BREAKING CHANGE: `src/validations.ts` renamed to `src/diagnostics.ts`
    
    BREAKING CHANGE: parser and validation interfaces changed significantly. please see `src/parsers.ts` and `src/diagnostics.ts` for the updated interfaces.

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,25 +1,72 @@
-import { Dictionary } from './basic';
-import { IValidationResult } from './validations';
+import { IDiagnostic } from './diagnostics';
 
-export type SourceMapParser<T = any> = (value: string) => IParserResult<T>;
+export type SourceMapParser<T = unknown, A extends object = object, L = unknown> = (
+  value: string
+) => IParserResult<T, A, L>;
 
-export type ObjPath = Array<string | number>;
+export type DocumentUri = string;
+export type Segment = string | number;
+export type JsonPath = Segment[];
+
+export interface IParserResult<T = any, A extends object = object, L = unknown> extends IParserASTResult<T, A, L> {
+  diagnostics: IDiagnostic[];
+}
+
+export interface IParserASTResult<T = unknown, A extends object = object, L = unknown> {
+  data: T;
+  ast: A;
+  lineMap: L;
+}
+
+export type GetJsonPathForPosition<A extends object, L = unknown> = (
+  result: IParserASTResult<unknown, A, L>,
+  position: IPosition
+) => JsonPath | undefined;
+
+// ILocation comes from the language server specification
+export type GetLocationForJsonPath<A extends object, L = unknown> = (
+  result: IParserASTResult<unknown, A, L>,
+  path: JsonPath
+) => ILocation | undefined;
 
 export interface IPosition {
-  /** line number (should start at 1) */
-  line: number;
-  column?: number;
+  /**
+   * Line position in a document (zero-based).
+   */
+  readonly line: number;
+
+  /**
+   * Character offset on a line in a document (zero-based). Assuming that the line is
+   * represented as a string, the `character` value represents the gap between the
+   * `character` and `character + 1`.
+   *
+   * If the character value is greater than the line length it defaults back to the
+   * line length.
+   */
+  readonly character: number;
 }
 
-export interface IPathPosition {
-  start: IPosition;
-  end?: IPosition;
+/**
+ * A range represents an ordered pair of two positions.
+ * It is guaranteed that start isBeforeOrEqual end.
+ */
+export interface IRange {
+  /**
+   * The start position. It is before or equal to end.
+   */
+  readonly start: IPosition;
+
+  /**
+   * The end position. It is after or equal to start.
+   */
+  readonly end: IPosition;
 }
 
-export type IParserResultPointers = Dictionary<IPathPosition, string>;
+export interface ILocation {
+  uri?: DocumentUri;
+  range: IRange;
+}
 
-export interface IParserResult<T = any> {
-  data: T;
-  pointers: IParserResultPointers;
-  validations: IValidationResult[];
+export interface IJsonLocation extends ILocation {
+  path: JsonPath;
 }
