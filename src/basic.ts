d77474ee7b5dea292559824991492ee380146ffd
Jakub Rożek <jakub@stoplight.io>
2019-03-23 21:27:11 +0100

    feat: add optional type

--- src/basic.ts
+++ src/basic.ts
@@ -25,3 +25,5 @@ export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 /** Easy create opaque types ie. types that are subset of their original types (ex: positive numbers, uppercased string) */
 export type Opaque<K, T> = T & { __TYPE__: K };
+
+export type Optional<T> = T | undefined;
