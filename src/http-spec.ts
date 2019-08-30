51e4e8d5dd4ffe615f415103ca9e9088b665ff84
Phil Sturgeon <me@philsturgeon.uk>
2019-08-30 13:38:54 +0200

    Revert "feat: enable comma limited parameters (#51)"
    This reverts commit 664f2be590c0a75da4d665c8dea3d27c10436c8c.

--- src/http-spec.ts
+++ src/http-spec.ts
@@ -83,7 +83,6 @@ export enum HttpParamStyles {
   Matrix = 'matrix',
   Label = 'label',
   Form = 'form',
-  CommaDelimited = 'commaDelimited',
   SpaceDelimited = 'spaceDelimited',
   PipeDelimited = 'pipeDelimited',
   DeepObject = 'deepObject',
@@ -97,10 +96,10 @@ export interface IHttpPathParam extends IHttpParam {
 export interface IHttpQueryParam extends IHttpParam {
   // should default to form
   style:
-  | HttpParamStyles.Form
-  | HttpParamStyles.SpaceDelimited
-  | HttpParamStyles.PipeDelimited
-  | HttpParamStyles.DeepObject;
+    | HttpParamStyles.Form
+    | HttpParamStyles.SpaceDelimited
+    | HttpParamStyles.PipeDelimited
+    | HttpParamStyles.DeepObject;
 
   allowEmptyValue?: boolean;
   allowReserved?: boolean;
@@ -135,11 +134,10 @@ export interface IHttpEncoding {
 
   // defaults to form
   style:
-  | HttpParamStyles.Form
-  | HttpParamStyles.CommaDelimited
-  | HttpParamStyles.SpaceDelimited
-  | HttpParamStyles.PipeDelimited
-  | HttpParamStyles.DeepObject;
+    | HttpParamStyles.Form
+    | HttpParamStyles.SpaceDelimited
+    | HttpParamStyles.PipeDelimited
+    | HttpParamStyles.DeepObject;
 
   headers?: IHttpHeaderParam[];
   mediaType?: string;
