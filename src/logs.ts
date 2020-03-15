379fd1b31d39762cf92cb6ab107d57937feab84b
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-03-14 23:15:17 -0500

    chore: remove this we aren't going to do

--- src/logs.ts
+++ src/logs.ts
@@ -15,13 +15,3 @@ export const enum LogLevelLabel {
   Error = 'error',
   Fatal = 'fatal',
 }
-
-// TODO: more formal logging system, a couple of options to do the heavy lifting? something like this?
-// https://github.com/pinojs/pino
-// export interface ILog<T = any> {
-//   level: LogLevel;
-//   msg: string;
-//   time: number;
-//   source?: string;
-//   data?: T;
-// }
