72f1c1819a3c5591bc8b5520916b3ec2a2fbe321
Vincenzo Chianese <vincenz.chianese@icloud.com>
2020-03-16 14:20:27 -0500

    feat: export ut types

--- src/basic.ts
+++ src/basic.ts
@@ -1,4 +1,4 @@
-/** Adapted from https://github.com/krzkaczor/ts-essentials */
+export { DeepPartial, DeepReadonly } from 'utility-types'
 
 /** Essentials */
 export type Primitive = string | number | boolean | undefined | null;
@@ -6,19 +6,6 @@ export type Primitive = string | number | boolean | undefined | null;
 /** Dictionaries related */
 export type Dictionary<T, K extends string | number = string> = { [key in K]: T };
 export type DictionaryValues<T> = T extends Dictionary<infer U> ? U : never;
-
-/** Like Partial but recursive */
-export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends Array<infer U>
-    ? Array<DeepPartial<U>>
-    : T[P] extends ReadonlyArray<infer U2> ? ReadonlyArray<DeepPartial<U2>> : DeepPartial<T[P]>
-};
-
-/** Like Readonly but recursive */
-export type DeepReadonly<T> = T extends Primitive
-  ? T
-  : T extends Array<infer U> ? ReadonlyArray<U> : T extends Function ? T : DeepReadonlyObject<T>;
-type DeepReadonlyObject<T> = { readonly [P in keyof T]: DeepReadonly<T[P]> };
 
 /** Easy create opaque types ie. types that are subset of their original types (ex: positive numbers, uppercased string) */
 export type Opaque<K, T> = T & { __TYPE__: K };
