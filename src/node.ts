7573cfffa2ef20159f7665932d2af13d2a2a140c
Thomas Pytleski <pytlesk4@gmail.com>
2020-06-24 00:45:07 -0500

    feat: add raw node type so we can store it in db
    required for https://github.com/stoplightio/platform-internal/issues/1464

--- src/node.ts
+++ src/node.ts
@@ -2,19 +2,20 @@
  * Stoplight node types
  */
 export enum NodeType {
-  Article = 'article',
-  HttpService = 'http_service',
-  HttpServer = 'http_server',
-  HttpOperation = 'http_operation',
-  Model = 'model',
-  Unknown = 'unknown',
+  Article = "article",
+  HttpService = "http_service",
+  HttpServer = "http_server",
+  HttpOperation = "http_operation",
+  Model = "model",
+  Raw = "raw",
+  Unknown = "unknown",
 }
 
 /**
  * Node data formats
  */
 export enum NodeFormat {
-  Json = 'json',
-  Markdown = 'markdown',
-  Yaml = 'yaml',
+  Json = "json",
+  Markdown = "markdown",
+  Yaml = "yaml",
 }
