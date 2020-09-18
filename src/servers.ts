5aee35c0f773cb91cf77f9d6a97ebbb2ad657949
William Hilton <wmhilton@gmail.com>
2020-09-17 22:12:57 -0400

    feat: server.id

--- src/servers.ts
+++ src/servers.ts
@@ -2,6 +2,7 @@ import { Dictionary } from './basic';
 import { INodeVariable } from './graph';
 
 export interface IServer {
+  id: string;
   url: string;
   name?: string;
   description?: string;
